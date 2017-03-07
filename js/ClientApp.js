6b5d898e26635d42e2fd512d687cde72b9d5bb9e
mr-jungchoi <mr.jungchoi@gmail.com>
2017-03-07 14:23:54 -0500

    Setup webpack server

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MyTitle from './MyTitle'
+import { render } from 'react-dom'
+import { HashRouter, Match } from 'react-router'
+import Landing from './Landing'
+import '../public/normalize.css'
+import '../public/style.css'
 
-var MyFirstComponent = React.createClass({
-  render: function() {
+const App = React.createClass({
+  render () {
     return (
-      <div>
-        <MyTitle title='this is a title' color='grey' />
-        <MyTitle title='this is another one' color='rebeccapurple' />
-        <MyTitle title='why are there so many titles' color='tomato' />
-        <MyTitle title='this is the last one' color='peru' />
-      </div>
+      <HashRouter>
+        <div className='app'>
+          <Match exactly pattern='/' component={Landing} />
+        </div>
+      </HashRouter>
     )
   }
 })
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+render(<App />, document.getElementById('app'))
