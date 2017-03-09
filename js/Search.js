1d0c22ed04210e2c7b6501adc0081968c1bbe332
mr-jungchoi <mr.jungchoi@gmail.com>
2017-03-09 16:18:00 -0500

    Complete search functionality with filter

--- js/Search.js
+++ js/Search.js
@@ -14,14 +14,16 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
+        <header>
+          <h1>svideo</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
         <div>
-          <header>
-            <h1>svideo</h1>
-            <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
-          </header>
-        </div>
-        <div>
-          {preload.shows.map((show) => {
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map((show) => {
             return (
               <ShowCard key={show.imdbID} {...show} />
             )
